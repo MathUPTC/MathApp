73e04272f5f54ccd2767bb35f0c427820c79f9fc
Fernando Leon <fernandoleonsierra@gmail.com>
2024-10-23 18:29:12 -0500

    rutas iniciales

--- app/app.py
+++ app/app.py
@@ -1,11 +1,36 @@
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-
+# Ruta para la página de inicio
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
-    
-if __name__=='__main__':
+
+# Ruta para la sección "Sobre el Proyecto"
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+# Ruta para la sección "Código de Conducta"
+@app.route('/codigo-de-conducta')
+def codigo_de_conducta():
+    return render_template('codigo_de_conducta.html')
+
+# Ruta para la sección "Recursos"
+@app.route('/recursos')
+def recursos():
+    return render_template('recursos.html')
+
+# Ruta para la sección "Contacto"
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+# Ruta para la sección "Inicio"
+@app.route('/inicio')
+def inicio():
+    return render_template('index.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
